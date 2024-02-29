7908a009699c2692aabf8f2b174376a250768f37
Billy Runne <b.runne@student.tudelft.nl>
2024-02-29 17:41:46 +0100

    Added methods getAll and getById

--- server/src/main/java/server/api/ParticipantController.java
+++ server/src/main/java/server/api/ParticipantController.java
@@ -1,10 +1,13 @@
 package server.api;
 
+import commons.Quote;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import commons.Participant;
 import server.database.ParticipantRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/participants")
@@ -39,6 +42,28 @@ public class ParticipantController {
 
     private static boolean isNullOrEmpty(String s) {
         return s == null || s.isEmpty();
+    }
+
+    /**
+     * Mapping for getting a participant by id
+     * @param id the id of the participant to get
+     * @return the participant found or a bad request if no participant can be found
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Participant> getById(@PathVariable("id") long id) {
+        if (id < 0 || !repo.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(repo.findById(id).get());
+    }
+
+    /**
+     * Mapping for getting all participants
+     * @return a list of all participants
+     */
+    @GetMapping(path = { "", "/" })
+    public List<Participant> getAll() {
+        return repo.findAll();
     }
 
 }
