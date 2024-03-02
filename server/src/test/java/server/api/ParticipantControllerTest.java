a8e7d164bc3524d246711c823b1f607faf883f65
Paras Khan <pkhan@student.tudelft.nl>
2024-03-02 15:12:45 +0100

    Make tests for all ParticipantController methods

---
+++ server/src/test/java/server/api/ParticipantControllerTest.java
@@ -0,0 +1,78 @@
+package server.api;
+
+import commons.Event;
+import commons.Participant;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import server.database.EventRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParticipantControllerTest {
+
+    private TestEventRepository eventRepo;
+    private TestParticipantRepository partRepo;
+    private ParticipantController sut;
+
+    private Event testEvent1;
+    private Participant testP1;
+
+
+
+    @BeforeEach
+    void setup() {
+        eventRepo = new TestEventRepository();
+        partRepo = new TestParticipantRepository();
+        sut = new ParticipantController(partRepo, eventRepo);
+        testEvent1 = new Event("testEvent1");
+        testEvent1.setId(100L);
+        testEvent1.addParticipant("testP1");
+        testP1 = testEvent1.getParticipants().get(0);
+        testP1.setId(500L);
+
+    }
+
+    @Test
+    void addParticipantTest() {
+        var retPart = sut.add(testP1, testEvent1.getId());
+        assertEquals(retPart.getBody(), testP1);
+
+    }
+
+    @Test
+    void getByIdTest() {
+        sut.add(testP1, testEvent1.getId());
+        var retPart = sut.getById(testP1.getId(), testEvent1.getId());
+        assertEquals(retPart.getBody(), testP1);
+
+    }
+
+    @Test
+    void getByIdNullTest() {
+        var retPart = sut.getById(testP1.getId(), testEvent1.getId());
+        assertNull(retPart.getBody());
+    }
+
+    @Test
+    void getAll() {
+        eventRepo.save(testEvent1);
+        var retList = sut.getAll(testEvent1.getId());
+        assertEquals(retList.getBody(), testEvent1.getParticipants());
+    }
+
+    @Test
+    void changeName() {
+        sut.add(testP1, testEvent1.getId());
+        sut.changeName("changeTest1", 500L, 100L);
+        assertEquals(testP1.getName(), sut.getById(testP1.getId(), testEvent1.getId()).getBody().getName());
+    }
+
+    @Test
+    void removeParticipant() {
+        eventRepo.save(testEvent1);
+        sut.add(testP1, testEvent1.getId());
+        var retDelete = sut.removeParticipant(testP1.getId(), testEvent1.getId());
+        assertEquals(retDelete.getBody(), testP1);
+        assertEquals(0, sut.getAll(testEvent1.getId()).getBody().size());
+    }
+}
