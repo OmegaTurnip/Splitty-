57cb9f8cdd448372f1fac550414ccd544afb54a1
Paras Khan <pkhan@student.tudelft.nl>
2024-03-10 17:34:55 +0100

    Writing up some more tests and improving previous ones.

--- client/src/test/java/client/utils/ServerUtilsTest.java
+++ client/src/test/java/client/utils/ServerUtilsTest.java
@@ -1,13 +1,13 @@
 package client.utils;
 
+
 import commons.Event;
 import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.Invocation;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import javafx.util.Builder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -24,7 +24,7 @@ class ServerUtilsTest {
     private String mockURL;
 
     @Mock
-    private  UserConfig USER_SETTINGS;
+    private UserConfig USER_SETTINGS;
 
     @Mock
     private Client client;
@@ -39,23 +39,48 @@ class ServerUtilsTest {
     void setup() {
         MockitoAnnotations.openMocks(this);
         mockURL = "www.mockTest.com/";
-        sut = new ServerUtils(mockURL); // <- Currently doesn't work and needs fixing!
+        sut = new ServerUtils(USER_SETTINGS, mockURL, client);
+
 
     }
 
     @Test
     void createEventTest() {
+        //Mocking the URL
+        when(USER_SETTINGS.getServerUrl()).thenReturn(mockURL);
+
+        // Mock behavior of client
+        when(client.target(anyString())).thenReturn(webTarget);
+
+        // Mocking WebTarget
+        when(webTarget.path(anyString())).thenReturn(webTarget);
+        when(webTarget.queryParam(anyString(), anyList())).thenReturn(webTarget);
+
+        // Mocking Invocation.Builder
+        when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(builder);
+        when(builder.accept(MediaType.APPLICATION_JSON)).thenReturn(builder);
+
+        //Mocking Response
+        Event testEvent1 = new Event("testEvent1");
+        when(builder.post(Entity.entity(testEvent1, MediaType.APPLICATION_JSON_TYPE), Event.class)).thenReturn(testEvent1);
+
+        Event event = sut.createEvent(testEvent1);
+
+        assertEquals(event, testEvent1);
+
     }
 
     @Test
     void getMyEventsTest() {
-        List<String> invCodes = Arrays.asList("INV1", "INV2");
 
+        //Mocking the URL
         when(USER_SETTINGS.getServerUrl()).thenReturn(mockURL);
+
         //Mocking the event codes
+        List<String> invCodes = Arrays.asList("INV1", "INV2");
         when(USER_SETTINGS.getEventCodes()).thenReturn(invCodes);
 
-        // Mocking Client
+        // Mock behavior of client
         when(client.target(anyString())).thenReturn(webTarget);
 
         // Mocking WebTarget
@@ -68,14 +93,15 @@ class ServerUtilsTest {
 
 
         // Mocking Response
-        List<Event> mockEvents = Arrays.asList(new Event("Test1"), new Event("Test2"));
-        Response response = mock(Response.class);
-        when(response.readEntity(any(GenericType.class))).thenReturn(mockEvents);
-        when(builder.get()).thenReturn(response);
+        Event test1 = new Event("Test1");
+        Event test2 = new Event("Test2");
+        List<Event> mockEvents = Arrays.asList(test1, test2);
+        when(builder.get(any(GenericType.class))).thenReturn(mockEvents);
 
         List<Event> events = sut.getMyEvents();
 
         assertEquals(events.size(), 2);
+        assertEquals(events.get(0), test1);
 
     }
 }
