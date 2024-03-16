a5fe889eeab755285030c9a171e93230c5c871f5
Esther Wit <e.f.n.wit@student.tudelft.nl>
2024-03-16 17:00:22 +0100

    Test for transactionController

---
+++ server/src/test/java/server/api/TestTransactionRepository.java
@@ -0,0 +1,319 @@
+package server.api;
+
+import commons.Event;
+import commons.Participant;
+import commons.Transaction;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import server.database.TransactionRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class TestTransactionRepository implements TransactionRepository {
+
+    private List<Transaction> added = new ArrayList<>();
+    /**
+     * @param transactionName The transaction name
+     * @return
+     */
+    @Override
+    public Transaction findTransactionByTransactionName(String transactionName) {
+        return null;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void flush() {
+
+    }
+
+    /**
+     * @param entity
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    /**
+     * @param entities
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    /**
+     * @param entities
+     */
+    @Override
+    public void deleteAllInBatch(Iterable<Transaction> entities) {
+
+    }
+
+    /**
+     * @param longs
+     */
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> longs) {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    /**
+     * @param aLong
+     * @return
+     */
+    @Override
+    public Transaction getOne(Long aLong) {
+        return null;
+    }
+
+    /**
+     * @param aLong
+     * @return
+     */
+    @Override
+    public Transaction getById(Long aLong) {
+        return null;
+    }
+
+    /**
+     * @param aLong
+     * @return
+     */
+    @Override
+    public Transaction getReferenceById(Long aLong) {
+        return null;
+    }
+
+    /**
+     * @param example
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    /**
+     * @param example
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    /**
+     * @param example
+     * @param sort
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    /**
+     * @param example
+     * @param pageable
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    /**
+     * @param example
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> long count(Example<S> example) {
+        return 0;
+    }
+
+    /**
+     * @param example
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    /**
+     * @param example
+     * @param queryFunction
+     * @param <S>
+     * @param <R>
+     * @return
+     */
+    @Override
+    public <S extends Transaction, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    /**
+     * @param entity
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> S save(S entity) {
+        added.add(entity);
+        return entity;
+    }
+
+    /**
+     * @param entities
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends Transaction> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public List<Transaction> findAll() {
+        return null;
+    }
+
+    /**
+     * @param longs
+     * @return
+     */
+    @Override
+    public List<Transaction> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    /**
+     * @param aLong
+     */
+    @Override
+    public void deleteById(Long aLong) {
+        for (Transaction t : added) {
+            if (t.getId().equals(aLong)) {
+                added.remove(t);
+                return;
+            }
+        }
+
+
+    }
+
+    /**
+     * @param entity
+     */
+    @Override
+    public void delete(Transaction entity) {
+
+    }
+
+    /**
+     * @param longs
+     */
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    /**
+     * @param entities
+     */
+    @Override
+    public void deleteAll(Iterable<? extends Transaction> entities) {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void deleteAll() {
+
+    }
+
+    /**
+     * @param aLong
+     * @return
+     */
+    @Override
+    public Optional<Transaction> findById(Long aLong) {
+        Optional<Transaction> found = Optional.empty();
+        for (Transaction t : added) {
+            if (t.getId().equals(aLong)) {
+                found = Optional.of(t);
+                break;
+            }
+        }
+        return found;
+    }
+
+    /**
+     * @param aLong
+     * @return
+     */
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    /**
+     * @param sort
+     * @return
+     */
+    @Override
+    public List<Transaction> findAll(Sort sort) {
+        return null;
+    }
+
+    /**
+     * @param pageable
+     * @return
+     */
+    @Override
+    public Page<Transaction> findAll(Pageable pageable) {
+        return null;
+    }
+}
+
---
+++ server/src/test/java/server/api/TransactionControllerTest.java
@@ -0,0 +1,90 @@
+package server.api;
+
+import commons.Event;
+import commons.Participant;
+import commons.Transaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TransactionControllerTest {
+
+
+    private TestEventRepository eventRepo;
+    private TestTransactionRepository transactionRepo;
+
+    private TransactionController sut;
+
+    private Event testEvent1;
+    private Transaction transaction;
+
+    private Transaction editTransaction;
+
+    private Participant testP1;
+
+    private List<Participant> group;
+
+    @BeforeEach
+    void setup() {
+        eventRepo = new TestEventRepository();
+        transactionRepo = new TestTransactionRepository();
+        sut = new TransactionController(transactionRepo, eventRepo);
+        testEvent1 = new Event("testEvent1");
+        testEvent1.setId(100L);
+        testEvent1.addParticipant("testP1");
+        testP1 = testEvent1.getParticipants().getFirst();
+        testP1.setId(500L);
+        group = new ArrayList<>();
+        group.add(testP1);
+        transaction = testEvent1.registerTransaction(testP1, "testTransaction1", 100, group, null);
+        transaction.setId(600L);
+        editTransaction = new Transaction(testP1, "editTransaction",  100, group, testEvent1,null);
+
+    }
+
+    @Test
+    void getTransactionById() {
+        eventRepo.save(testEvent1);
+        sut.addTransaction(testEvent1.getId(), transaction);
+        var retPart = sut.getTransaction(testEvent1.getId(), transaction.getId());
+        assertEquals(retPart.getBody(), transaction);
+    }
+
+    @Test
+    void getTransactionIdIsNull(){
+        var retPart = sut.getTransaction(testEvent1.getId(), transaction.getId());
+        assertNull(retPart.getBody());
+    }
+
+    @Test
+    void editTransaction() {
+        eventRepo.save(testEvent1);
+        sut.addTransaction(testEvent1.getId(), transaction);
+        var retPart = sut.editTransaction(testEvent1.getId(), transaction.getId(), editTransaction);
+        assertEquals(retPart.getBody().getTransactionName(), editTransaction.getTransactionName());
+        assertEquals(retPart.getBody().getPrice(), editTransaction.getPrice());
+        assertEquals(retPart.getBody().getPayer(), editTransaction.getPayer());
+        assertEquals(retPart.getBody().getParticipants(), editTransaction.getParticipants());
+        assertEquals(retPart.getBody().getDate(), editTransaction.getDate());
+        assertEquals(retPart.getBody().getTag(), editTransaction.getTag());
+        assertEquals(retPart.getBody().getEvent(), editTransaction.getEvent());
+    }
+
+    @Test
+    void addTransaction() {
+        var retPart = sut.addTransaction(testEvent1.getId(), transaction);
+        assertEquals(retPart.getBody(), transaction);
+    }
+
+    @Test
+    void deleteTransaction() {
+        eventRepo.save(testEvent1);
+        sut.addTransaction(testEvent1.getId(), transaction);
+        var retPart = sut.deleteTransaction(testEvent1.getId(), transaction.getId());
+        assertEquals(retPart.getBody(), transaction);
+    }
+}
